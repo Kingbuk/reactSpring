8079dd130ba3998d752e715686fd594c541c6412
Kingbuk <119140340+Kingbuk@users.noreply.github.com>
2025-03-05 23:35:51 +0900

    0305

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
+import data from 'mocks/latest-board-list.mock';
+import latestBoardListMock from 'mocks/latest-board-list.mock';
+import Top3Item from 'components/top3item';
+import { top3BoardListMock } from 'mocks';
 
 function App() {
   return (
-    <></>
+  <>
+    <div style={{display : 'flex', justifyContent:'center', gap : '24px'}}>
+      {top3BoardListMock.map(top3ListItem => <Top3Item top3Listitem={top3ListItem}/>)}
+    </div>
+  </>
   );
 }
 
